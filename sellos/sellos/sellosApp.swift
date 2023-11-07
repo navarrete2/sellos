124d96ae4d6d223030b6ec72e195caa1ac260d54
navarrete2 <m4gu4l@icloud.com>
2023-11-07 22:00:21 +0100

    utilizacion de git con xcode

---
+++ sellos/sellos/sellosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sellosApp.swift
+//  sellos
+//
+//  Created by Miguel  on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct sellosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
